09845afce5ccb464371b09ce9b9392464dbe666b
Ben Payne <@>
2022-05-31 22:46:11 -0400

    Various tab-to-space conversions and additional TODO comments to remind me what to do

--- gangPulse.js
+++ gangPulse.js
@@ -1,38 +1,40 @@
+// TODO change to be a list of tasks passed by arg, that steps through and
+//   assigns next task in list, looping back to first at end
 let argSchema = [
-	['tail', false],
-	['help', false],
-	['h', false],
-	['once', false],
-	['loopTime', 2.5], // minutes
-	['exclude', []],
-	['hackGang', false],
-	// ['verbose', false],
+    ['tail', false],
+    ['help', false],
+    ['h', false],
+    ['once', false],
+    ['loopTime', 2.5], // minutes
+    ['exclude', []],
+    ['hackGang', false],
+    // ['verbose', false],
 ]
 export function autocomplete(data, args) {
-	data.flags(argSchema);
-	return [];
+    data.flags(argSchema);
+    return [];
 }
 /** @param {NS} ns */
 export async function main(ns) {
-	let argFlags = ns.flags(argSchema);
-	if (argFlags.help || argFlags.h) {
-		ns.tprintf("INFO: TODO info about gangPulse.js");
-		ns.tprintf("INFO: run gangPulse.js --loopTime 1 --once --tail");
-		ns.exit();
-	}
-	let taskSwap1 = (argFlags.hackGang) ? "Train Hacking" : "Train Combat";
-	let taskSwap2 = "Territory Warfare";
-	do {
-		for (const gmName of ns.gang.getMemberNames()) {
-			if (!argFlags.exclude.includes(gmName)) {
-				let gmInfo = ns.gang.getMemberInformation(gmName);
-				if (gmInfo.task == taskSwap1) {
-					ns.gang.setMemberTask(gmName, taskSwap2);
-				} else if (gmInfo.task == taskSwap2) {
-					ns.gang.setMemberTask(gmName, taskSwap1);
-				}
-			}
-		}
-		if (!argFlags.once) await ns.sleep(argFlags.loopTime * 60 * 1000);
-	} while (!argFlags.once);
+    let argFlags = ns.flags(argSchema);
+    if (argFlags.help || argFlags.h) {
+        ns.tprintf("INFO: TODO info about gangPulse.js");
+        ns.tprintf("INFO: run gangPulse.js --loopTime 1 --once --tail");
+        ns.exit();
+    }
+    let taskSwap1 = (argFlags.hackGang) ? "Train Hacking" : "Train Combat";
+    let taskSwap2 = "Territory Warfare";
+    do {
+        for (const gmName of ns.gang.getMemberNames()) {
+            if (!argFlags.exclude.includes(gmName)) {
+                let gmInfo = ns.gang.getMemberInformation(gmName);
+                if (gmInfo.task == taskSwap1) {
+                    ns.gang.setMemberTask(gmName, taskSwap2);
+                } else if (gmInfo.task == taskSwap2) {
+                    ns.gang.setMemberTask(gmName, taskSwap1);
+                }
+            }
+        }
+        if (!argFlags.once) await ns.sleep(argFlags.loopTime * 60 * 1000);
+    } while (!argFlags.once);
 }
--- threadAnalyzer.js
+++ threadAnalyzer.js
@@ -1,45 +1,46 @@
 let argSchema = [
-	['help', false],
-	['tail', false],
-	['percent', 20],
-	['cores', 1], // TODO: pull cores from host being checked
+    ['help', false],
+    ['tail', false],
+    ['percent', 20],
+    ['cores', 1], // TODO: pull cores from host being checked
 ];
 export function autocomplete(data, args) {
-	data.flags(argSchema); // adds flags to autocomplete
-	return [...data.servers];
+    data.flags(argSchema); // adds flags to autocomplete
+    return [...data.servers];
 }
 
 /** @param {NS} ns */
 export async function main(ns) {
-	let argFlags = ns.flags(argSchema);
-	if (argFlags.help) {
-		ns.tprint("INFO: threadAnalyzer --help");
-		ns.tprint("TODO: INFO ABOUT threadAnalyzer");
-		ns.tprint("threadAnalyzer --percent # ");
-		ns.exit();
-	}
-	let sName = argFlags._[0];
-	let weakenVal = 0.05; // TODO: grab this from somewhere? it's the default 1 core value. but unsure how it factors with bit-nodes
-	let hPct = argFlags.percent / 100;
-	ns.tprintf("Running threadAnalyzer on %s for (%s) amount of the value",
-		sName,
-		ns.nFormat(hPct, "0%"),
-	);
-	// get number of hack threads to get hPct of the server (sName)
-	let hackThreads = hPct / ns.hackAnalyze(sName);
-	// get % hacked from hackThreads number of threads
-	let percentToGrow = hackThreads * ns.hackAnalyze(sName);
-	// get number of growth threads to compensate
-	let growthThreads = Math.ceil(ns.growthAnalyze(sName, 1 + percentToGrow, argFlags.cores));
-	// get total value needed to weaken from both hack and grow
-	let toWeaken = ns.hackAnalyzeSecurity(hackThreads, sName) + ns.growthAnalyzeSecurity(growthThreads);
-	// get threads by dividing by weakenVal (TODO can this be automated? not sure if 0.05 is ALWAYS the case)
-	let weakenThreads = Math.ceil(toWeaken / weakenVal);
-	let totalThreads = hackThreads + growthThreads + weakenThreads;
-	let formatString = "%-13s:\t%3i\t(%3s)";
-	ns.tprintf(formatString, "hackThreads", hackThreads, ns.nFormat(hackThreads / totalThreads, "0.0%"));
-	ns.tprintf(formatString, "growthThreads", growthThreads, ns.nFormat(growthThreads / totalThreads, "0.0%"));
-	ns.tprintf(formatString, "weakenThreads", weakenThreads, ns.nFormat(weakenThreads / totalThreads, "0.0%"));
-	ns.tprintf(formatString, "totalThreads", totalThreads, ns.nFormat(1, "0.0%"));
+    let argFlags = ns.flags(argSchema);
+    if (argFlags.help) {
+        ns.tprint("INFO: threadAnalyzer --help");
+        ns.tprint("TODO: INFO ABOUT threadAnalyzer");
+        ns.tprint("threadAnalyzer --percent # ");
+        ns.exit();
+    }
+    let sName = argFlags._[0];
+    let weakenVal = 0.05; // TODO: grab this from somewhere? it's the default 1 core value. but unsure how it factors with bit-nodes
+    let hPct = argFlags.percent / 100;
+    ns.tprintf("Running threadAnalyzer on %s for (%s) amount of the value",
+        sName,
+        ns.nFormat(hPct, "0%"),
+    );
+    // get number of hack threads to get hPct of the server (sName)
+    let hackThreads = hPct / ns.hackAnalyze(sName);
+    // get % hacked from hackThreads number of threads
+    let percentToGrow = hackThreads * ns.hackAnalyze(sName);
+    // TODO adjust percentToGrow to be a dollar value, then get a new percent based on new available money
+    // get number of growth threads to compensate
+    let growthThreads = Math.ceil(ns.growthAnalyze(sName, 1 + percentToGrow, argFlags.cores));
+    // get total value needed to weaken from both hack and grow
+    let toWeaken = ns.hackAnalyzeSecurity(hackThreads, sName) + ns.growthAnalyzeSecurity(growthThreads);
+    // get threads by dividing by weakenVal (TODO can this be automated? not sure if 0.05 is ALWAYS the case)
+    let weakenThreads = Math.ceil(toWeaken / weakenVal);
+    let totalThreads = hackThreads + growthThreads + weakenThreads;
+    let formatString = "%-13s:\t%3i\t(%3s)";
+    ns.tprintf(formatString, "hackThreads", hackThreads, ns.nFormat(hackThreads / totalThreads, "0.0%"));
+    ns.tprintf(formatString, "growthThreads", growthThreads, ns.nFormat(growthThreads / totalThreads, "0.0%"));
+    ns.tprintf(formatString, "weakenThreads", weakenThreads, ns.nFormat(weakenThreads / totalThreads, "0.0%"));
+    ns.tprintf(formatString, "totalThreads", totalThreads, ns.nFormat(1, "0.0%"));
 
 }
